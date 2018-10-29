63718cc2f1f3b90639a3e05f9619caafa208bf10
Kelvin <44570273+kks110@users.noreply.github.com>
2018-10-29 13:58:29 +0100

    Add files via upload

---
+++ timestamp-ping-limited.ps1
@@ -0,0 +1,21 @@
+﻿
+#IP address to ping
+$ip = Read-Host -Prompt 'Where would you like to ping?'
+
+#Rounds of 10 pings
+$rounds = Read-Host -Prompt 'How many sets of 10 pings would you like to do?'
+
+# Output file
+$file = "C:\pings\" + $ip + ".txt"
+
+if (-Not (Test-Path "C:\pings"))
+{
+New-Item C:\pings -type directory -Force | Out-Null
+}
+
+while ($i -lt $rounds){
+    get-date >> $file
+    ping $ip -n 10 | select-string -pattern "TTL=" >> $file
+    Write-Output `n | Out-File $file -Append
+    $i++
+} 
---
+++ timestamp-ping.ps1
@@ -0,0 +1,16 @@
+﻿#IP address to ping
+$ip = Read-Host -Prompt 'Where would you like to ping?'
+
+# Output file
+$file = "C:\pings\" + $ip + ".txt"
+
+if (-Not (Test-Path "C:\pings"))
+{
+New-Item C:\pings -type directory -Force | Out-Null
+}
+
+while ($true){
+    get-date >> $file
+    ping $ip -n 10 | select-string -pattern "TTL=" >> $file
+    Write-Output `n | Out-File $file -Append
+} 
