2a81fe6b0f1d79016205790954b59b8fef3107a3
Kelvin <44570273+kks110@users.noreply.github.com>
2018-10-29 14:00:23 +0100

    Add files via upload

--- timestamp-ping-limited.ps1
+++ timestamp-ping-limited.ps1
@@ -1,21 +1,50 @@
-﻿
-#IP address to ping
-$ip = Read-Host -Prompt 'Where would you like to ping?'
+﻿#IP address to ping.
+$ip = Read-Host -Prompt "Where would you like to ping?"
 
-#Rounds of 10 pings
-$rounds = Read-Host -Prompt 'How many sets of 10 pings would you like to do?'
 
-# Output file
-$file = "C:\pings\" + $ip + ".txt"
+#Determines if its IPv4 or 6.
+if ($ip -like '*:*') {
+    $ipv = "-6"
+} else {
+    $ipv = "-4"
+}
 
+
+#If the directory doesn't exist, it creates it.
 if (-Not (Test-Path "C:\pings"))
 {
 New-Item C:\pings -type directory -Force | Out-Null
 }
 
+
+# Output file name.
+if ($ip -like '*:*'){
+    $file = "C:\pings\IPv6_Pingtest.txt"
+} else {
+    $file = "C:\pings\" + $ip + ".txt"
+}
+
+
+# Checks to see if the file already exsist and asks if you want to delete it.
+if (Test-Path $file) {
+    "It looks like " + $file + " already exists"
+    $YorN = Read-Host -Prompt "would you like to delete it? (if no, it will append) (Y/N)"
+    if ($YorN -like '*y*') {
+        Remove-Item -path $file
+    }
+}
+
+#Rounds of 10 pings.
+$rounds = Read-Host -Prompt 'How many sets of 10 pings would you like to do?'
+
+
+#Pings and outputs to the file.
 while ($i -lt $rounds){
     get-date >> $file
-    ping $ip -n 10 | select-string -pattern "TTL=" >> $file
+    ping $ip $ipv -n 10 | select-string -pattern Reply,Request >> $file
     Write-Output `n | Out-File $file -Append
     $i++
+    "Round " + $i + " of pings complete."
 } 
+
+"Script complete. Please see " + $file + " for results."
--- timestamp-ping.ps1
+++ timestamp-ping.ps1
@@ -1,16 +1,44 @@
-﻿#IP address to ping
+﻿#IP address to ping.
 $ip = Read-Host -Prompt 'Where would you like to ping?'
 
-# Output file
-$file = "C:\pings\" + $ip + ".txt"
 
+#Determines if its IPv4 or 6.
+if ($ip -like '*:*') {
+    $ipv = "-6"
+} else {
+    $ipv = "-4"
+}
+
+
+#If the directory doesn't exist, it creates it.
 if (-Not (Test-Path "C:\pings"))
 {
 New-Item C:\pings -type directory -Force | Out-Null
 }
 
+
+# Output file name.
+if ($ip -like '*:*'){
+    $file = "C:\pings\IPv6_Pingtest.txt"
+} else {
+    $file = "C:\pings\" + $ip + ".txt"
+}
+
+
+# Checks to see if the file already exsist and asks if you want to delete it.
+if (Test-Path $file) {
+    "It looks like " + $file + " already exists"
+    $YorN = Read-Host -Prompt "would you like to delete it? (if no, it will append) (Y/N)"
+    if ($YorN -like '*y*') {
+        Remove-Item -path $file
+    }
+}
+
+
+#Pings and outputs to the file.
+"Running test, please press Ctrl + C, or close this window to stop it"
 while ($true){
     get-date >> $file
-    ping $ip -n 10 | select-string -pattern "TTL=" >> $file
+    ping $ip $ipv -n 100 | select-string -pattern Reply,Request >> $file
     Write-Output `n | Out-File $file -Append
 } 
