e4c1b5131f59e945d6c56f81a97bd938bae94276
Kelvin <44570273+kks110@users.noreply.github.com>
2018-11-26 23:58:25 +0000

    Removing duplicate code
    Removing duplicate code

--- timestamp-ping-limited.ps1
+++ timestamp-ping-limited.ps1
@@ -2,14 +2,6 @@
 $ip = Read-Host -Prompt "Where would you like to ping?"
 
 
-#Determines if its IPv4 or 6.
-if ($ip -like '*:*') {
-    $ipv = "-6"
-} else {
-    $ipv = "-4"
-}
-
-
 #If the directory doesn't exist, it creates it.
 if (-Not (Test-Path "C:\pings"))
 {
@@ -17,13 +9,14 @@ New-Item C:\pings -type directory -Force | Out-Null
 }
 
 
-# Output file name.
-if ($ip -like '*:*'){
+#Determines if its IPv4 or 6 and determines the file name.
+if ($ip -like '*:*') {
+    $ipv = "-6"
     $file = "C:\pings\IPv6_Pingtest.txt"
 } else {
+    $ipv = "-4"
     $file = "C:\pings\" + $ip + ".txt"
 }
-
 
 # Checks to see if the file already exsist and asks if you want to delete it.
 if (Test-Path $file) {
